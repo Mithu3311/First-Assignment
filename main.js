556f005c3b01351482084b4de3ee2ae9792fe359
Mithu <142711288+Mithu3311@users.noreply.github.com>
2024-02-27 12:10:16 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var favoriteColor = "red";
+console.log(favoriteColor);
+favoriteColor = "Green";
+console.log(favoriteColor);
+var PascalCase = "red";
+var PersonName = "Mithu";
+console.log(PersonName);
---
+++ main.ts
@@ -0,0 +1,10 @@
+let favoriteColor = "red";
+console.log(favoriteColor)
+favoriteColor = "Green";
+console.log(favoriteColor)
+
+let PascalCase = "red";
+
+let PersonName = "Mithu";
+
+console.log(PersonName);
